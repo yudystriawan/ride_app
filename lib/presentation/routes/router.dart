e60d266703415e95dadae389e97ffe3a738842fd
yudystriawan <yudistriawan@gmail.com>
2021-01-02 05:39:21 +0800

    add splash page

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route_annotations.dart';
+import 'package:ride_app/presentation/pages/rider/login/login_page.dart';
 import 'package:ride_app/presentation/pages/splash/splash_page.dart';
 
 @AdaptiveAutoRouter(
   routes: [
     AdaptiveRoute(page: SplashPage, initial: true),
+    AdaptiveRoute(page: LoginPage),
   ],
 )
 class $Router {}
